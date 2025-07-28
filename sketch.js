079f7fc120ed0452756fb13ead825a603d045447
lucs-max <luccaandradebonifacio@gmail.com>
2025-07-28 18:14:10 -0400

    adicionei os meus sprites na programacao do jogo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 var sapo, sapoAnimation;
 var aranha, aranhaAnimation;
 var cobra, cobraAnimation;
+var cinos, cinosAnimation;
+var nyjag, nyjagAnimation;
 
 function preload(){
   sapoAnimation = loadAnimation("./assets/sapojump0.png", "./assets/sapojump1.png", "./assets/sapojump2.png", "./assets/sapojump3.png", "./assets/sapojump4.png", "./assets/sapojump5.png", "./assets/sapojump6.png");
   cobraAnimation = loadAnimation("./assets/cobra0.png", "./assets/cobra1.png", "./assets/cobra2.png", "./assets/cobra3.png", "./assets/cobra4.png");
   aranhaAnimation = loadAnimation("./assets/aranha0.png", "./assets/aranha1.png", "./assets/aranha2.png", "./assets/aranha3.png");
+  cinosAnimation = loadAnimation("./assets/Cinos0.png","./assets/Cinos1.png","./assets/Cinos2.png");
+  nyjagAnimation = loadAnimation("./assets/nyjag_0.png","./assets/nyjag_1.png","./assets/nyjag_2.png","./assets/nyjag_3.png");
 }
 
 function setup() {
@@ -16,6 +20,10 @@ function setup() {
   aranha.addAnimation("aranhaAnimation", aranhaAnimation);
   cobra = createSprite(600, 100);
   cobra.addAnimation("cobraAnimation", cobraAnimation);
+  cinos = createSprite(900,100);
+  cinos.addAnimation("cinosAnimation", cinosAnimation);
+  nyjag = createSprite(1200,100);
+  nyjag.addAnimation("nyjagAnimation",nyjagAnimation);
 }
 
 function draw() {
