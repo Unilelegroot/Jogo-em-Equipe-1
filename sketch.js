1841cad52c5bf0afc6291e331c52725a2f5acf6b
dejandrea <dejandrea@gmail.com>
2025-08-04 20:18:18 -0300

    alterações do Lucca

--- sketch.js
+++ sketch.js
@@ -12,23 +12,23 @@ var inimigosGroup;
 var inimigos;
 
 
-function preload(){
+function preload() {
   ceuImage = loadImage("./assets/ceu.png");
   sapoAnimationJump = loadAnimation("./assets/sapojump0.png", "./assets/sapojump1.png", "./assets/sapojump2.png", "./assets/sapojump3.png", "./assets/sapojump4.png", "./assets/sapojump5.png", "./assets/sapojump6.png");
   sapoAnimation = loadAnimation("./assets/saporun0.png", "./assets/saporun1.png", "./assets/saporun2.png", "./assets/saporun3.png");
   cobraAnimation = loadAnimation("./assets/cobra0.png", "./assets/cobra1.png", "./assets/cobra2.png", "./assets/cobra3.png", "./assets/cobra4.png");
   aranhaAnimation = loadAnimation("./assets/aranha0.png", "./assets/aranha1.png", "./assets/aranha2.png", "./assets/aranha3.png");
-  cinosAnimation = loadAnimation("./assets/Cinos0.png","./assets/Cinos1.png","./assets/Cinos2.png");
-  nyjagAnimation = loadAnimation("./assets/nyjag_0.png","./assets/nyjag_1.png","./assets/nyjag_2.png","./assets/nyjag_3.png");
+  cinosAnimation = loadAnimation("./assets/Cinos0.png", "./assets/Cinos1.png", "./assets/Cinos2.png");
+  nyjagAnimation = loadAnimation("./assets/nyjag_0.png", "./assets/nyjag_1.png", "./assets/nyjag_2.png", "./assets/nyjag_3.png");
   formigaAnimation = loadAnimation("./assets/formiga0.png", "./assets/formiga1.png", "./assets/formiga2.png", "./assets/formiga3.png", "./assets/formiga4.png", "./assets/formiga5.png");
   gramaImage = loadImage("./assets/grama.png");
-  morcegoAnimation = loadAnimation("./assets/morcego1.png","./assets/morcego2.png","/assets/morcego3.png");
+  morcegoAnimation = loadAnimation("./assets/morcego1.png", "./assets/morcego2.png", "/assets/morcego3.png");
 }
 
 function setup() {
   createCanvas(1420, 500);
 
-  ceu = createSprite(600,100);
+  ceu = createSprite(600, 100);
   ceu.addImage(ceuImage);
 
   grama = createSprite(710, 250);
@@ -43,13 +43,9 @@ function setup() {
   sapo.changeAnimation("correndo");
   sapo.scale = 0.45;
 
-  aranha = createSprite((grama.x/2)+100, 220);
+  aranha = createSprite((grama.x / 2)+227, 220);
   aranha.addAnimation("aranhaAnimation", aranhaAnimation);
 
-  inimigo = createSprite(700, 400);
-  inimigo.addAnimation("formigaAnimation", formigaAnimation);
-  inimigo.addAnimation("cobraAnimation", cobraAnimation);
-  inimigo.addAnimation("cinosAnimation", cinosAnimation);
   // cinos = createSprite(900,400);
   // cinos.mirrorX(-1);
   // cinos.scale = 2.25;
@@ -65,7 +61,7 @@ function setup() {
 
   // morcego = createSprite(600, 230);
   // morcego.addAnimation("morcegoVoando", morcegoAnimation);
-}  
+}
 
 function draw() {
   background("lightgrey");
@@ -81,11 +77,11 @@ function draw() {
   // }
   // if(nyjag.y>=261){
   //   nyjag.velocityY=-2;
-    
+
   // }
   // gravidade no sapo
   sapo.velocityY += 0.8;
-  sapo.collide(gramaInvisivel);  
+  sapo.collide(gramaInvisivel);
   // pulo
   if (keyDown("space") && sapo.y >= 360) {
     sapo.velocityY = -12;
@@ -110,25 +106,27 @@ function draw() {
   text("X: " + mouseX + " / Y: " + mouseY, mouseX, mouseY);
 }
 
-function gerarInimigosSolo(){
-  if(frameCount%100==0){
-    //cobra.x = 1440;
-    var rand = Math.round(random(1,3));
+function gerarInimigosSolo() {
+  var randFrame = Math.round(random(80, 120));
+  if (frameCount % randFrame == 0) {
+    inimigo = createSprite(width+50, 400);
+    inimigo.velocityX = -4.2;
+    var rand = Math.round(random(1, 3));
     //console.log(rand)
-    switch(rand){
-      case 1: 
-     inimigo.scale = 0.43
-      inimigo.changeAnimation("cobraAnimation", cobraAnimation);
-      break;
-      case 2: 
-      inimigo.mirrorX(-1);
-      inimigo.scale = 2.25 
-        inimigo.changeAnimation("cinosAnimation", cinosAnimation);
+    switch (rand) {
+      case 1:
+        inimigo.scale = 0.43
+        inimigo.addAnimation("cobraAnimation", cobraAnimation);
         break;
-       case 3: 
-       inimigo.scale = 1.3 
-        inimigo.changeAnimation("formigaAnimation", formigaAnimation);
-      break;
+      case 2:
+        inimigo.mirrorX(-1);
+        inimigo.scale = 2.25
+        inimigo.addAnimation("cinosAnimation", cinosAnimation);
+        break;
+      case 3:
+        inimigo.scale = 1.3
+        inimigo.addAnimation("formigaAnimation", formigaAnimation);
+        break;
     }
   }
 }
